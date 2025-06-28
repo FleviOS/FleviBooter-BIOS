de33de3cff4e6f6da26e27bd53931de83c1f4749
mimmeer-github <ajcbpw2@gmail.com>
2025-06-28 11:53:16 +0100

    Stacked the LEGO bricks.
    <(Hello from mimmeer-github)
    
    I have finally completed building the stack up.
    
    CHANGELOG:
    
    ~ /code/biosstg.asm
    
    NOTE:
    These mini-0changelogs are just for finding changed files, not for what changed.

--- code/biosstg.asm
+++ code/biosstg.asm
@@ -12,6 +12,30 @@
 org 0x7C00            ; Where we start
 bits 16               ; This is real mode (16-bits) right now.
 
+; DEFINITIONS
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+puts:
+    push si           ; Saving the unmodified registers
+    push ax           ; Done.
+
+.loop:                ; INTO THE LOOPAGONS!
+    lodsb             ; Loads the thing.
+    or al, al         ; Oring AL to AL and storing it in AL. (AL counter: 6)
+    jz .done          ; Jumping through the Nulliverse for function.done.
+
+    mov ah, 0x0E      ; E.
+    mov bh, 0         ; Null Bicycle
+    int 0x10          ; We are sorry to interrupt this CPU process to speak to Mr. BIOS
+
+.done:
+    pop ax            ; POP!
+    pop si            ; Where the bubbles going?
+    ret               ; RETurn to the cosmos!
+    
 
 main:                 ; Our starting point
     mov ax,0          ; Data segments
@@ -19,12 +43,19 @@ main:                 ; Our starting point
     mov es,ax         ; Done!
 
     mov ss,ax         ; Stack setup time!
-    mov p,0x7C00      ; Stack setup complete!
-    hlt               ; Instant CPU stop
+    mov sp,0x7C00     ; Stack setup complete!
 
-.halt:                ; In case the CPU decides to unhalt.
+    mov si, msg_hello ; Code <(Hello!)
+    call puts         ; I'll puts this on the screen.
+
+
+    hlt               ; EMERGENCY FOUND! ENABLE INSTANT CPU STOP!
+
+.halt:                ; In case the CPU decides to unhsalt.
     jmp .halt         ; INFINITE LOOP
 
+msg_hello: 'Hello the vast FOSS world to Flevi!', ENDL
+
 times 510-($-$$) db 0 ; Padding galore!
-db 055h             ; Special BIOS sign PART I!
-db 0AAh             ; Special BIOS sign PART II!
+db 055h               ; Special BIOS sign PART I!
+db 0AAh               ; Special BIOS sign PART II!
